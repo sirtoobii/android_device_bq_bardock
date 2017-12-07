634caacb7c6e3925ed80f432181c3f93b59d7a42
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    bardock: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo beta blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -180,6 +180,11 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libcurl
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
