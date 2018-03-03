e02462d4f9564975814be16ae9039ead82501c31
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-03 01:53:31 -0500

    bardock: Add gatekeeper HAL

--- device.mk
+++ device.mk
@@ -155,6 +155,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
