c143be11c370ba17dcccccb481bb4aaf6e5ccb25
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-03 01:54:47 -0500

    bardock: Add keymaster HAL

--- device.mk
+++ device.mk
@@ -183,6 +183,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8953
