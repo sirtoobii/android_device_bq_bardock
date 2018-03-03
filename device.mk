46051e6e948f8ffa6103156a6dcd05fdc67c7a0a
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-03 01:48:30 -0500

    bardock: Add graphics HALs

--- device.mk
+++ device.mk
@@ -131,6 +131,13 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8953 \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
